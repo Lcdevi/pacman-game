191775934bbeab8324d3efbd810f27750f3765e1
Lcdevi <caillauxlaura@gmail.com>
2020-09-05 20:18:53 +0200

    create grid, create colors for each different type of square

---
+++ script/script.js
@@ -0,0 +1,36 @@
+    const mainContent = document.getElementById('main-content');
+    const grid = [
+        ['0','0','0','0','0','0','0','0','0','0','0','0'],
+        ['4','4','0','0','0','0','0','0','0','0','4','4'],
+        ['0','4','0','1','4','0','0','0','0','0','4','0'],
+        ['0','4','0','4','4','4','4','4','4','4','4','0'],
+        ['0','0','0','0','0','0','0','0','0','0','0','0'],
+        ['0','0','0','0','0','0','0','0','0','0','0','0'],
+        ['0','0','0','0','0','0','0','0','0','0','0','0'],
+        ['0','0','0','0','0','0','0','0','0','0','0','0'],
+        ['0','0','0','0','0','0','0','0','0','0','0','0'],
+        ['0','0','0','0','0','0','0','0','0','0','0','0']
+    ]
+;
+
+
+
+function createGrid () {
+    for (let i = 0; i<grid.length; i++) {
+        for (let j = 0; j<grid[i].length; j++) {
+      let newDiv = document.createElement("div");
+      newDiv.classList.add('div')
+      mainContent.appendChild(newDiv);
+      if (grid[i][j] === '0') {
+        newDiv.classList.add('wall')
+    } else if (grid[i][j] === '4') {
+        newDiv.classList.add('chemin')
+    } else if (grid[i][j] === '1') {
+        newDiv.classList.add('life')
+    }
+        }
+      }
+
+    }
+
+    createGrid();
