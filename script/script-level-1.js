8765c8c72595a6f00094265eac8654ae2e02eab4
Lcdevi <caillauxlaura@gmail.com>
2020-09-07 12:33:36 +0200

    level 1 : done. Move walker : ok. Take life score +1 : done. Remove life class when took : done. Open wall when walker passes on open-wall class : done. Alert You win when access the exit with +2 score : done.

--- script/script-level-1.js
+++ script/script-level-1.js
@@ -1,7 +1,8 @@
 const mainContent = document.querySelector('.main-content');
 mainContent.classList.add('main-content-level-1');
-let score = document.getElementById('score');
-
+let displayScore = document.getElementById('score');
+let score = 1;
+displayScore.innerHTML = score;
 //push toutes les divs dans cet array pour pouvoir retrouver chaque div avec un index.
 //const arrayOfDiv = []; 
 // plus besoin de cet array puisque je récupere toutes les div dans une nodelist
@@ -15,7 +16,7 @@ const walker = {
 const grid = [
     ['0','0','0','0','0','0','0','0','0','0'],
     ['0','0','0','0','1','0','0','0','0','0'],
-    ['4','4','4','4','4','4','4','4','4','0'],
+    ['4','4','4','4','4','4','3','4','4','0'],
     ['0','0','0','0','0','0','0','0','0','0']
 ];
 
@@ -73,15 +74,26 @@ window.addEventListener('keydown', (event) => {
    // arrayOfDiv[20].classList.remove('walker')
     if (event.key === 'ArrowRight') { 
         console.log('right press');
+        if(grid[walker.x][walker.y] === grid[2][9] && score === 2) {
+            alert("you win")
+        }
         if(grid[walker.x][walker.y+1] === '4'||grid[walker.x][walker.y+1] === '1') {
         grid[walker.x][walker.y] = '4';
         walker.y++;
         grid[walker.x][walker.y] = "5";
+        } else if(grid[walker.x][walker.y+1] === '3') {
+            grid[walker.x][walker.y] = '4';
+            walker.y++;
+            grid[walker.x][walker.y] = "5";
+            grid[2][9] = '4';
+            nodeListOfDivs[29].classList.add('chemin'); 
         }
+
+       
     } 
     if (event.key === 'ArrowLeft') { 
         console.log('left press');
-        if(grid[walker.x][walker.y-1] === '4'||grid[walker.x][walker.y-1] === '1' ) {
+        if(grid[walker.x][walker.y-1] === '4'||grid[walker.x][walker.y-1] === '1'||grid[walker.x][walker.y-1] === '3') {
         grid[walker.x][walker.y] = '4';
         walker.y--;
         grid[walker.x][walker.y] = "5";
@@ -94,12 +106,13 @@ window.addEventListener('keydown', (event) => {
             grid[walker.x][walker.y] = '4';
             walker.x--;
             grid[walker.x][walker.y] = "5";
-        } else if (grid[walker.x-1][walker.y] === '1') {
+        } 
+        if (grid[walker.x-1][walker.y] === '1') {
             grid[walker.x][walker.y] = '4';
             walker.x--;
             grid[walker.x][walker.y] = "5";
             score = score + 1;
-            console.log(score);
+            displayScore.innerHTML = score;
         }
     } 
     if (event.key === 'ArrowDown') { 
@@ -117,6 +130,10 @@ window.addEventListener('keydown', (event) => {
             nodeListOfDivs[(9*i)+i+j].classList.add('walker');
         } else if (grid[i][j] === '4') {
             nodeListOfDivs[(9*i)+i+j].classList.remove('walker');
+            nodeListOfDivs[(9*i)+i+j].classList.remove('life'); 
+        } else if (grid[i][j] === '3') {
+            nodeListOfDivs[(9*i)+i+j].classList.remove('walker');
+            nodeListOfDivs[(9*i)+i+j].classList.add('open-wall'); 
         }
     }
 }
